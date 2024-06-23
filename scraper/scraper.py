f222ee2709b6fb37ac76cb0f441bf51fe39c9b59
Saumya Shah <47592405+snshahgit@users.noreply.github.com>
2024-06-22 23:59:47 -0400

    Update scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -3,194 +3,295 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 import time
 import random
-import traceback
-from pymongo.errors import BulkWriteError
-from mongodb_connection import get_database
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-import requests
 import json
+import traceback
 
 def login(driver, username, password):
-    try:
-        login_button = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.CLASS_NAME, "combinedLoginLinkWrapper"))
-        )
-        login_button.click()
 
-        email = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.CLASS_NAME, "email"))
-        )
-        email.send_keys(username)
 
-        next_button = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.CLASS_NAME, "button.Button.submitButton.primary"))
-        )
-        next_button.click()
+    # print('login')
+    loginbutton = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "combinedLoginLinkWrapper"))
+    )
+    loginbutton.click()
 
-        password_field = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.CLASS_NAME, "password"))
-        )
-        password_field.send_keys(password)
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "guts"))
+    )
 
-        submit_button = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.CLASS_NAME, "button.Button.submitButton.v3.primary"))
-        )
-        submit_button.click()
+    email = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "email"))
+    )
+    email.send_keys(username)
 
-        WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.ID, "userMenu"))
-        )
-    except (TimeoutException, NoSuchElementException, WebDriverException) as e:
-        print(f"Error during login: {e}")
-        raise e
+    next = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "button.Button.submitButton.primary"))
+    ).click()
 
-def get_all_rental_properties(driver, pin, house_type):
-    try:
-        WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "HomeViews"))
-        )
-        property_list = driver.find_elements(By.CLASS_NAME, "bp-Homecard__Content")
-        data = get_data(driver, property_list, pin, house_type)
-        return data
-    except (TimeoutException, WebDriverException) as e:
-        print(f"Error while fetching rental properties: {e}")
-        return []
+    passkey = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "password"))
+    )
+    passkey.send_keys(password)
 
-def get_data(driver, property_list, pin, house_type):
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "button.Button.submitButton.v3.primary"))
+    ).click()
+
+
+
+
+def get_to_listings(driver):
+    # print('listings')
+
+    # Wait for the search bar to be present
+    searchbar = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "search-box-input"))
+    )
+    searchbar.clear()
+    searchbar.send_keys("Jersey City" + Keys.ENTER)
+    # print('done typing')
+    # Wait for the search button to be clickable and click it
+    # print('Searched')
+
+
+def get_all_rental_properties(driver, pin, house_type, safety, weather):
+    # waiting till all placards are fetchable
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "HomeViews"))
+    )
+
+    property_list = driver.find_elements(By.CLASS_NAME, "bp-Homecard__Content")
+    # print(len(property_list))
+    
+    data = get_data(driver, property_list, pin, house_type, safety, weather)
+    return data
+
+def get_weather(driver, pin, base_url):
+    
+    driver.get(f"https://riskfactor.com/zip/00000-mi/{pin}_fsid/flood")
+    flood = WebDriverWait(driver, 60).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".my-4 > div:nth-child(1) > p:nth-child(1) > span:nth-child(2)"))
+    ).text
+    # print(flood)
+
+    driver.get(f"https://riskfactor.com/zip/00000-mi/{pin}_fsid/fire")
+    fire = WebDriverWait(driver, 60).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".pt-2 > div:nth-child(1) > p:nth-child(3) > span:nth-child(2)"))
+    ).text
+    # print(fire)
+
+    driver.get(f"https://riskfactor.com/zip/00000-mi/{pin}_fsid/wind")
+    wind = WebDriverWait(driver, 60).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".pt-2 > div:nth-child(1) > p:nth-child(1) > span:nth-child(1)"))
+    ).text
+    # print(wind)
+
+    driver.get(f"https://riskfactor.com/zip/00000-mi/{pin}_fsid/air")
+    air = WebDriverWait(driver, 60).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".pt-2 > div:nth-child(1) > p:nth-child(1) > span:nth-child(2)"))
+    ).text
+    # print(air)
+
+    driver.get(f"https://riskfactor.com/zip/00000-mi/{pin}_fsid/heat")
+    heat = WebDriverWait(driver, 60).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".pt-2 > div:nth-child(1) > p:nth-child(1) > span:nth-child(2)"))
+    ).text
+    # print(heat)
+
+    driver.get(base_url)
+    
+    weather = [flood, fire, wind, air, heat]
+
+    return weather
+
+def get_crime(driver, pin, base_url):
+
+    driver.get("https://crimegrade.org/")
+    zipcode = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "FormBodyInput"))
+    )
+    zipcode.send_keys(pin + Keys.RETURN)
+    crime = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "overallGradeLetter"))
+    )
+    crime = crime.text
+    crime_range = ['A+','A','A-','B+','B','B-','C+','C','C-','D+','D','D-','E+','E','E-','F+','F','F-']
+    safety = ((len(crime_range) - crime_range.index(crime))/len(crime_range))
+    driver.get(base_url)
+    return safety
+
+# get data like address, price, beds, baths, sqft, parking space, images, price per square feet
+def get_data(driver, property_list, pin, house_type, safety, weather):
+
     data = []
     original_window = driver.current_window_handle
-
     for property in property_list:
-        try:
-            property.click()
-            WebDriverWait(driver, 20).until(EC.number_of_windows_to_be(2))
-            new_window = [window for window in driver.window_handles if window != original_window][0]
-            driver.switch_to.window(new_window)
 
-            address = WebDriverWait(driver, 20).until(
+        property.click()
+        # Wait for the new tab to open
+        WebDriverWait(driver, 30).until(EC.number_of_windows_to_be(2))
+        # Switch to the new tab
+        new_window = [window for window in driver.window_handles if window != original_window][0]
+        driver.switch_to.window(new_window)
+
+        address = None
+        try:
+            # Perform actions on the new tab
+            address = WebDriverWait(driver, 30).until(
                 EC.presence_of_element_located((By.CLASS_NAME, "full-address"))
             ).text
+            # print(address)
+        except:
+            print("No information on the address!")
 
-            price, beds, baths, sqft = [val.text for val in WebDriverWait(driver, 20).until(
-                EC.presence_of_all_elements_located((By.CLASS_NAME, "statsValue"))
-            )]
 
-            build, parking_space, price_per_sqft, hoa_fee = None, None, None, None
+        [price, beds, baths, sqft] = [val.text for val in WebDriverWait(driver, 20).until(
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "statsValue"))
+        )]
+
+
+        build, parking_space, price_per_sqft, hoa_fee = None, None, None, None
+        try:
             parent_elements = driver.find_elements(By.CLASS_NAME, "keyDetails-row")
 
             for parent in parent_elements:
-                if 'Built in' in parent.text and not build:
+                if 'Built in' in parent.text and build is None:
                     build = parent.text
-                if 'parking space' in parent.text and not parking_space:
-                    parking_space = parent.text
-                if 'per sq ft' in parent.text and not price_per_sqft:
-                    price_per_sqft = parent.text
-                if 'HOA fee' in parent.text and not hoa_fee:
-                    hoa_fee = parent.text
+                    # print(f"Built in: {build}")
 
-            tax_info = None
-            parent_elements = driver.find_elements(By.CLASS_NAME, "amenity-group")
+                if 'parking space' in parent.text and parking_space is None:
+                    parking_space = parent.text
+                    # print(f"Parking Space: {parking_space}")
+
+                if 'per sq ft' in parent.text and price_per_sqft is None:
+                    price_per_sqft = parent.text
+                    # print(f"Price per Sq Ft: {price_per_sqft}")
+
+                if 'HOA fee' in parent.text and hoa_fee is None:
+                    hoa_fee = parent.text
+                    # print(f"HOA fee: {hoa_fee}")
+        except:
+            print("No information on Build, Parking space, Price per square ft, and HOA fee!")
+
+
+        tax_info = None
+        try:
+
+            parent_elements = WebDriverWait(driver, 20).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "amenity-group"))
+            )
+
             for parent in parent_elements:
                 if "Tax" in parent.text:
                     tax_info = parent.text
 
-            image_elements = driver.find_elements(By.CSS_SELECTOR, "img.landscape")
+            # print(tax_info)
+        except:
+            print('Could not locate tax information!')
+
+        image_urls = None
+        try: 
+            image_elements = WebDriverWait(driver, 60).until(
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, "img.landscape"))
+            )
+            # Extract URLs from image elements
             image_urls = [element.get_attribute('src') for element in image_elements]
+        except:
+            print("No images to show!")
+        # print(image_urls)
+        
+        transitScore, sideWalkScore = None, None
+        try:
+            amenities = WebDriverWait(driver, 40).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "percentage"))
+            )
+            sideWalkScore = amenities[0].text
+            transitScore = amenities[1].text
+            # print(transitScore, sideWalkScore)
+        except:
+            print('No information on public transit and sidewalks!')
 
-            data.append({
-                'pin': pin,
-                'houseType': house_type,
-                'address': address,
-                'price': price,
-                'beds': beds,
-                'baths': baths,
-                'sqft': sqft,
-                'parking': parking_space,
-                'construction': build,
-                'pricePerSqft': price_per_sqft,
-                'homeOwnersAssociationFees': hoa_fee,
-                'taxInfo': tax_info,
-                'imgUrls': image_urls
-            })
+        schoolCount = None
+        try:
+            schools = WebDriverWait(driver, 40).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "ItemPickerPill__label--count"))
+            )
+            schoolCount = [school.text for school in schools][0]
+            # print(schoolCount)
+        except:
+            print("No school information")
 
-            driver.close()
-            driver.switch_to.window(original_window)
-        except (TimeoutException, NoSuchElementException, WebDriverException) as e:
-            print(f"Error while fetching property data: {e}")
-            driver.close()
-            driver.switch_to.window(original_window)
+        data.append({'pin':pin, 'houseType':house_type, 'address':address, 'price':price, 'beds':beds, 'baths':baths, 'sqft':sqft, 'parking':parking_space, 'construction':build, 'pricePerSqft':price_per_sqft, 'homeOwnersAssociationFees':hoa_fee, 'taxInfo':tax_info, 'imgUrls':image_urls, 'transitScore':transitScore, 'sideWalkScore':sideWalkScore, 'schoolCount':schoolCount, 'safety':safety, 'weather':weather})
 
+        driver.close()
+        driver.switch_to.window(original_window)        
+        break
     return data
 
-def retry(func, max_attempts=3, *args, **kwargs):
-    attempts = 0
-    while attempts < max_attempts:
-        try:
-            result = func(*args, **kwargs)
-            return result
-        except (TimeoutException, WebDriverException) as e:
-            print(f"Error occurred: {e}. Retrying {attempts + 1}/{max_attempts}...")
-            attempts += 1
-            time.sleep(5)  # Adding delay between retries
-    print(f"Failed after {max_attempts} attempts.")
-    return None
 
-def scrape_properties(pin_codes):
-    server_url = 'http://localhost:5000/add_properties'
+def main():
+    try:
+        # sample url: base url/zipcode/{pin}/filter/property-type={house_type},min-beds={min_beds},min-price={min_price},max-price={max_price}
+        # default value for pin
+        pin = '07201'
+        # House type: House, Townhouse, Condo
+        house_type = random.choice(['house', 'townhouse', 'condo'])
+        # Budget 50K to 10M (max-price)
+        min_price= None # '50K'
+        max_price = None # '2M'
+        # Minimum beds
+        min_beds= 1 # 2.5
 
-    for pin in pin_codes:
+        # Initialize the Chrome driver
+
         chrome_options = Options()
         chrome_options.add_argument('--ignore-certificate-errors')
         chrome_options.add_argument('--ignore-ssl-errors')
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-        driver.set_page_load_timeout(60)
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.set_page_load_timeout(60)  # Adjust timeout as needed
         url = 'https://www.redfin.com/'
 
         username = 'shahsau1@msu.edu'
         password = 'P@ssw0rd'
         driver.get(url)
-        
-        retry(login, 3, driver, username, password)
+        login(driver, username, password)
 
-        try:
-            house_type = random.choice(['house', 'townhouse', 'condo'])
-            driver.get(f"https://www.redfin.com/zipcode/{pin}")
-            data = retry(get_all_rental_properties, 3, driver, pin, house_type)
-            if data:
-                try:
-                    response = requests.post(server_url, json=data)
-                    if response.status_code == 200:
-                        print(f"Successfully inserted data for pin code {pin}")
-                    else:
-                        print(f"Failed to insert data for pin code {pin}: {response.text}")
-                except Exception as e:
-                    print(f"An error occurred while sending data for pin code {pin} to the server: {e}")
-                    traceback.print_exc()
-            time.sleep(random.uniform(5, 10))  # Random sleep to avoid getting blocked
-        except (TimeoutException, WebDriverException) as e:
-            print(f"Error occurred for pin code {pin}: {e}")
-        except Exception as e:
-            print(f"An unexpected error occurred for pin code {pin}: {e}")
-            traceback.print_exc()
-        finally:
-            driver.quit()
 
-def main():
-    file_path = 'zipcodes.txt'
-    try:
-        with open(file_path, 'r') as file:
-            pin_codes = [line.strip() for line in file.readlines()]
-            print(pin_codes)
-            scrape_properties(pin_codes)
-    except FileNotFoundError:
-        print(f"File not found: {file_path}")
-    except Exception as e:
-        print(f"An error occurred while reading the file: {e}")
+        # Ensure the login process is complete
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.ID, "userMenu"))
+        )
 
-if __name__ == "__main__":
+        safety = get_crime(driver, pin, base_url= url)
+        # print(safety)
+        weather = get_weather(driver, pin, base_url= url)
+        print(safety, weather)
+        url+= f"zipcode/{pin}/filter/property-type={house_type},min-beds={min_beds},min-price={min_price},max-price={max_price}"
+        # print(url)
+        # Navigate to URL using driver.navigate().to(url)
+        driver.get(url)
+        data = get_all_rental_properties(driver, pin, house_type, safety, weather)
+
+        print(json.dumps(data))
+
+
+
+
+        # Optional: Wait for the results page to load
+        time.sleep(10)  # You might want to replace this with a more specific wait
+
+    except TimeoutException:
+        print("Loading took too much time!")
+
+    finally:
+        # Close the driver
+        driver.quit()
+
+
+
+if __name__=="__main__":
     main()
